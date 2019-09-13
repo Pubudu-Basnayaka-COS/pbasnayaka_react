4655e6257bcc1ea9de4bcc37bb36c8141284b136
Pubudu Basnayaka <Pubudu.Basnayaka@Saskatoon.ca>
2019-09-13 13:34:21 -0600

    added avatar and animation

--- src/components/AvatarComponent.js
+++ src/components/AvatarComponent.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/_avatarcomponent.scss';
+import Avatar from "../images/avatar1.jpg";
 
 export default class AvatarComponent extends Component {
   render() {
+    var avatarStyle = {
+      backgroundImage: 'url(' + Avatar + ')',
+    };
     return (
-        <div className="avatar">
-        avatar
+        <div id="avatar-container">
+          <div className="avatar" style={avatarStyle} >
+
+          </div>
+
         </div>
     )
   }
 }
+
