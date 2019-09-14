a197eb5b621ce67d0033f48f5530c75b8ec66651
Pubudu Basnayaka <downcastx@gmail.com>
2019-09-13 21:54:18 -0600

    creative item title being displayed woo

--- src/components/CreativeWorkComponent.js
+++ src/components/CreativeWorkComponent.js
@@ -4,19 +4,29 @@ import axios from 'axios';
 
 
 export default class CreativeWorkComponent extends Component {
+    state = {
+        creativework: []
+    }
     componentDidMount() {
         axios.get(`https://pbasnayaka.com/jsonapi/node/creative_work`)
             .then(res => {
-                const data = res.data;
+                const data = res.data.data;
                 console.log(data)
-               // this.setState({ persons });
+                this.setState({ creativework : data });
             })
     }
 
     render() {
         return (
             <div>
-                Creative Work Component stuff
+                {this.state.creativework.map((work_item) => (
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">{work_item.attributes.title}</h3>
+
+                        </div>
+                    </div>
+                ))}
             </div>
         )
     }
