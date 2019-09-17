a9c6e90323f61cdb3b4825f308942edb57376d18
Pubudu Basnayaka <downcastx@gmail.com>
2019-09-16 21:30:08 -0600

    change hover state for avatar based on navl inks

--- src/App.js
+++ src/App.js
@@ -4,25 +4,16 @@ import './styles/_main.scss';
 //components
 import NavComponent from './components/NavComponent';
 import ContentComponent from './components/ContentComponent';
-import AvatarComponent from './components/AvatarComponent';
+import SidebarComponent from './components/SidebarComponent';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
+
     return (
         <Router>
             <div id="main-container" className="App">
                 <div id="inner-container">
-                    <div id="sidebar-container">
-                        <div id="sidebar">
-                            <div className="sidebar-top">
-                                <AvatarComponent/>
-                            </div>
-                            <div className="sidebar-bottom">
-                                <NavComponent/>
-
-                            </div>
-                        </div>
-                    </div>
+                    <SidebarComponent/>
                     <div id="sidebar-padding">
 
                     </div>
