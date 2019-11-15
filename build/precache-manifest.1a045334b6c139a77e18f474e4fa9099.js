44d8e84fffacc6cd8670df1200e7783fbde92ef6
Pubudu Basnayaka <Pubudu.Basnayaka@Saskatoon.ca>
2019-11-15 12:41:56 -0600

    added public build

---
+++ build/precache-manifest.1a045334b6c139a77e18f474e4fa9099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5008de76b2e397e8eff7a3753f9e90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2507fb820f87b8b12203",
+    "url": "/static/css/main.78a262d7.chunk.css"
+  },
+  {
+    "revision": "2fb9387be324cfb486b8",
+    "url": "/static/js/2.3ede52c7.chunk.js"
+  },
+  {
+    "revision": "2507fb820f87b8b12203",
+    "url": "/static/js/main.5cd5ab65.chunk.js"
+  },
+  {
+    "revision": "b053cd394b1b9c98ccf4",
+    "url": "/static/js/runtime~main.bce2c8c6.js"
+  },
+  {
+    "revision": "8a2ba67fa97039b4602dcc36c61c6fde",
+    "url": "/static/media/avatar3.8a2ba67f.jpg"
+  }
+]);
